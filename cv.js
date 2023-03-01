996745f35153a00a47aecc64c9f0035a4a55e523
pdiez <pdiez@mp.gba.gov.ar>
2023-03-01 10:56:06 -0300

    nuevo

--- cv.js
+++ cv.js
@@ -1,10 +1,12 @@
 window.onscroll = function(){
     if(document.documentElement.scrollTo > 100){
-        document.querySelector('.go-top-container').classList.add('show')
-        document.querySelector('.go-top-container').addEventListener('click',()=>{
-            window.scrollTo({top:0,behavior:'smooth'})
-        })
+        document.querySelector('.go-top-container').classList.add('show')       
     }
+    
     else{document.querySelector('.go-top-container').classList.remove('show')
     }
 }
+
+$(document).on('click','#go-top',function(e){
+    $(document).scrollTop(0);
+})
