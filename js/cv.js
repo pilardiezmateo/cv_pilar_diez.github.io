7f65ce340d0d09018c8b9c6b11f648b6041ac65f
pdiez <pdiez@mp.gba.gov.ar>
2023-03-02 10:31:10 -0300

    js arreglado

--- js/cv.js
+++ js/cv.js
@@ -1,12 +1,3 @@
-window.onscroll = function(){
-    if(document.documentElement.scrollTo > 100){
-        document.querySelector('.go-top-container').classList.add('show')       
-    }
-    
-    else{document.querySelector('.go-top-container').classList.remove('show')
-    }
-}
-
 $(document).on('click','#go-top',function(e){
     $(document).scrollTop(0);
 })
