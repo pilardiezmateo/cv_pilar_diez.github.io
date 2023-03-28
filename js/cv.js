37cea03b69a19cb948dfcc85e448a51f71f1c07a
pdiez <pdiez@mp.gba.gov.ar>
2023-03-28 15:31:33 -0300

    js

--- js/cv.js
+++ js/cv.js
@@ -1,3 +1,5 @@
 $(document).on('click','#go-top',function(e){
     $(document).scrollTop(0);
 })
+$(document).on('click', '#boton',function () {
+    document.getElementById("personal_info").style.display = 'block';});
